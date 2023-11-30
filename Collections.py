e16ddf5a3ffe89a1efa3e8dba9982e411f622dac
blueflower77 <dev.gj.shurashetty@gmail.com>
2023-11-30 14:31:25 -0500

    Initial Commit

---
+++ Collections.py
@@ -0,0 +1,21 @@
+myFruitList=["Apple", "Grapes", "Banana"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[1]="Orange"
+print(myFruitList)
+myTuple=("Apple", "Banana", "Orange")
+print(myTuple)
+print(type(myTuple))
+print(myTuple[0])
+print(myTuple[1])
+myFavoriteFruitDictionary = {
+      "Akua" : "apple",
+      "Saanvi" : "banana",
+      "Paulo" : "pineapple"
+    }
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
---
+++ string-data-type.py
@@ -0,0 +1,17 @@
+# myString = "This is a String"
+# print(myString)
+# print(type(myString))
+# print(str(myString) + " is of the data type " + str(type(myString)))
+# firstString="Water"
+# secondString="fall"
+# thirdString=firstString + secondString
+# print(thirdString)
+# name = input("What is your name? ")
+# print(name)
+# color = input("What is your favorite Color? ")
+# animal = input("What is your favorite animal? ")
+# print("{}, you like a {} {}!".format(name,color,animal))
+name=input("What is your name? ")
+place=input("Where are you from? ")
+love=input("What do you love the most? ")
+print("{}, you love {} from {}!".format(name,love,place))
