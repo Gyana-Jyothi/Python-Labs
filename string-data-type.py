7df4bc301e7a4a96f7bd317d2220a0436e45e280
blueflower77 <dev.gj.shurashetty@gmail.com>
2023-12-01 22:41:53 -0500

    uncommented the code

--- string-data-type.py
+++ string-data-type.py
@@ -1,16 +1,16 @@
-# myString = "This is a String"
-# print(myString)
-# print(type(myString))
-# print(str(myString) + " is of the data type " + str(type(myString)))
-# firstString="Water"
-# secondString="fall"
-# thirdString=firstString + secondString
-# print(thirdString)
-# name = input("What is your name? ")
-# print(name)
-# color = input("What is your favorite Color? ")
-# animal = input("What is your favorite animal? ")
-# print("{}, you like a {} {}!".format(name,color,animal))
+myString = "This is a String"
+print(myString)
+print(type(myString))
+print(str(myString) + " is of the data type " + str(type(myString)))
+firstString="Water"
+secondString="fall"
+thirdString=firstString + secondString
+print(thirdString)
+name = input("What is your name? ")
+print(name)
+color = input("What is your favorite Color? ")
+animal = input("What is your favorite animal? ")
+print("{}, you like a {} {}!".format(name,color,animal))
 name=input("What is your name? ")
 place=input("Where are you from? ")
 love=input("What do you love the most? ")
